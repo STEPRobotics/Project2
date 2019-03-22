6ee4553313452fea8a1332dbb7f87df1c012435f
STEPRobotics <48580309+STEPRobotics@users.noreply.github.com>
2019-03-22 11:40:23 -0400

    Create project2.cpp

---
+++ project2.cpp
@@ -0,0 +1,32 @@
+#include <Servo.h>
+
+//TODOs:
+//Create an integer variable called sensor associated 
+//with its pin number. 
+
+//Create a servo variable called servo
+
+void setup(){
+  
+  //initialize the sensor using pinMode(pin#,INPUT)
+  
+  //initialize the servo using servo.attach()
+  
+  //Set the initial position of the servo to 0 
+  //by using Servo.write()
+  
+  
+}
+
+void loop(){
+  
+  //Find the sensor threshold value (Refer to photoresistor 
+  //and LED project.) 
+  
+  //Cretate an integer variable called sensorRead
+  //and set it equal to analogRead(sensor)
+  
+  //if sensorRead is higher than threshold, set the servo
+  //at 180 degrees, otherwise, set the servo at 0 degrees. 
+
+}
